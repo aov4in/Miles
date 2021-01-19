8f6b1947a80195622cd79986e194bbbaaf4bd902
Andrei Ovchinnikov <aovchin@gmail.com>
2021-01-19 11:23:29 +0300

    Added Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int priceticket = 1240;
+        int miles = 1;
+        int pricemiles = 20;
+        int amountmiles = priceticket / pricemiles * miles;
+
+        System.out.println(amountmiles);
+
+    }
+}
